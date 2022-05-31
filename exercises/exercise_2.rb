b65826f6bc6dcd5d7506f19f88e8639c258e19a8
Emma Grannis <grannis.emma@gmail.com>
2022-05-31 04:27:45 +0000

    Functioning exercises 1 and 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,17 +5,19 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
-@store1 = Store.find_by(id: 1)
-## can also just do Store.find(1) implicit that's it the ID. Usually find is exclusively for IDs becuase it req integers
+@store1 = Store.find(1)
+# Store.find(1) -- it's implicit that's it the ID. Usually find is exclusively for IDs becuase it req integers
+# can also do --> @store1 = Store.find_by(id: 1)
 
-@store2 = Store.find_by(id: 2)
+@store2 = Store.find(2)
 
-@store1.name = "Emma"
-# @store1.update(name: "Emma") # want to update the column of name, don't pass it a symbol
-# when passing a symbol, is usually when you are creating it
+
+@store1.update(name: "Emma") # want to update the column of name, so don't pass it a symbol
+# when you're passing a symbol, is usually when you are creating it
+
 puts @store1.name
 
-
+# -------
 # store1 = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
 # employee = Employee.create(first_name: "example", last_name: "lastnameexample", hourly_rate: 12, store_id: store1.id)
 
