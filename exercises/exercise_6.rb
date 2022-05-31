6ca4d5f7ebd6ad26c41c29a09ab77814813b64c2
Emma Grannis <grannis.emma@gmail.com>
2022-05-31 05:12:21 +0000

    Add store employee data in exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+# Add data into employees
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Alex", last_name: "Grannis", hourly_rate: 600)
+@store1.employees.create(first_name: "Monica", last_name: "Grannis", hourly_rate: 600)
+@store2.employees.create(first_name: "Jon", last_name: "Grannis", hourly_rate: 700)
+@store1.employees.create(first_name: "Saul", last_name: "Muskin", hourly_rate: 200)
+@store2.employees.create(first_name: "Kali", last_name: "Grannis", hourly_rate: 50)
