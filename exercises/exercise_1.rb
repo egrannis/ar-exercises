08d06eb5dab5dbc0bfee86a79c60d8a35628f701
Emma Grannis <grannis.emma@gmail.com>
2022-05-31 04:59:12 +0000

    Implement code for exercise 4

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -6,7 +6,7 @@ puts "----------"
 
 # Your code goes below here ...
 
-# active record's create class
+# active records create class
 store1 = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
 store2 = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 store3 = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
