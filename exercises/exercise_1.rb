131e606db98cc1b28dcb7bdab326424f2b968e6b
Emma Grannis <grannis.emma@gmail.com>
2022-05-31 00:57:26 +0000

    Fix error with how value is set in exercise 2

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -7,9 +7,11 @@ puts "----------"
 # Your code goes below here ...
 
 # active record's create class
-store = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
-store = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
-store = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+store1 = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+store2 = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+store3 = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
 # Output (puts) the number of the stores using ActiveRecord's count method, to ensure that there are three stores in the database
-puts Store.count(:all)
+# puts Store.count(:all)
+puts store2.id
+# puts Store.all.count other way to write this
